c414958a3fd53040ab80f7c530fa9aefa9ee966a
Abel Stanley <kumaken132@gmail.com>
2020-04-18 15:12:10 +0700

    Fixed depth, domain filtering, and tag selection

--- genericWebCrawler/genericWebCrawler/scraperLoader.py
+++ genericWebCrawler/genericWebCrawler/scraperLoader.py
@@ -6,15 +6,15 @@ def main():
     allow_domains = input("Allowed Domain: ")
     depth = input("Max Depth : ")
     result = loadScraper(root, allow_domains, depth)
-    # # print(result[0][2])
-    # for paragraph in result:
-    #     print("-------------------------------------")
-    #     print("PAGE :", paragraph[0])
-    #     print("[")
-    #     for sentence in paragraph[2]:
-    #         print("'"+sentence+"',")
-    #     print("]")
-    #     print("-------------------------------------")
+    # print(result[0][2])
+    for paragraph in result:
+        print("-------------------------------------")
+        print("PAGE :", paragraph[0])
+        print("[")
+        for sentence in paragraph[2]:
+            print("'"+sentence+"',")
+        print("]")
+        print("-------------------------------------")
 
 if __name__ == '__main__':
     main()
