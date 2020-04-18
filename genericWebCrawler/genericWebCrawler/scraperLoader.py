343048d707bd02e3acbb6003b66417cd6c427c10
Abel Stanley <kumaken132@gmail.com>
2020-04-18 15:10:51 +0700

    Fixed depth, domain filtering, and tag selection

--- genericWebCrawler/genericWebCrawler/scraperLoader.py
+++ genericWebCrawler/genericWebCrawler/scraperLoader.py
@@ -1,11 +1,20 @@
-from spiders.urlExtractor import loadScraper
+from spiders.crawler import loadScraper
+from pprint import pprint
 
 def main():
     root = input("Root : ")
     allow_domains = input("Allowed Domain: ")
     depth = input("Max Depth : ")
     result = loadScraper(root, allow_domains, depth)
-    print(result)
+    # # print(result[0][2])
+    # for paragraph in result:
+    #     print("-------------------------------------")
+    #     print("PAGE :", paragraph[0])
+    #     print("[")
+    #     for sentence in paragraph[2]:
+    #         print("'"+sentence+"',")
+    #     print("]")
+    #     print("-------------------------------------")
 
 if __name__ == '__main__':
     main()
--- genericWebCrawler/genericWebCrawler/spiders/urlExtractor.py
+++ genericWebCrawler/genericWebCrawler/spiders/urlExtractor.py
@@ -3,7 +3,10 @@ from scrapy import Request
 from scrapy.linkextractors import LinkExtractor
 from spacy.tokens import Token
 from spacy.lang.en import English # updated
-
+from urllib.parse import urlparse
+# Faster alternative? :
+# from nltk import tokenize
+# have to download punkt: python -m nltk.downloader 'punkt' OR go to python shell and type 'nltk.download('punkt')
 def create_crawler_class():
     Token.set_extension('tag', default=False)
 
@@ -21,8 +24,19 @@ def create_crawler_class():
             self.source = root
             self.options = kwargs
             self.depth = depth
+            self.traversedLinks = set()
             UrlExtractor.start_urls.append(root)
+            # print(self.options.get('allow_domains'))
+            if self.options.get('allow_domains') != '':
+                print("allowed domains set to given settings")
+            else:
+                print("allowed domains set to ROOT DOMAIN: ")
+                root_domain = urlparse(root).netloc
+                root_domain = '.'.join(root_domain.split('.')[1:])
+                self.options['allow_domains'] = root_domain
+                # print(self.options.get('allow_domains'))
             UrlExtractor.allowed_domains = [self.options.get('allow_domains')]
+
             self.clean_options()
             self.le = LinkExtractor(allow=self.options.get('allow'), deny=self.options.get('deny'),
                                     allow_domains=self.options.get('allow_domains'),
@@ -43,22 +57,25 @@ def create_crawler_class():
             title = response.xpath('//title//text()').extract()[0].strip()
 
             self.index += 1
-            # select all texts between tags except script tags:
-            temp = response.xpath('//*[not(self::script)]/text()[re:test(., "\w+")]').extract()
+            # select all texts between <p> tags except script tags: (double // to select all children too)
+            temp = response.xpath('//*[not(self::script) and not(self::a)]/p/text()[re:test(., "\w+")]').extract()
 
             listOfSentences = []
             for string in temp:
+                string = string.strip()
+                # print(string)
                 doc = self.nlp(string)
                 sentences = [sent.string.strip() for sent in doc.sents]
+                # print(sentences)
+                # sentences = tokenize.sent_tokenize(string)
                 listOfSentences.extend(sentences)
 
             all_urls = self.get_all_links(response)
-
+            self.traversedLinks = self.traversedLinks|all_urls
             # accumulate result:
             results.append((title, response.meta['url'], listOfSentences, all_urls))
 
-            all_urls = []
-            if int(response.meta['depth']) <= int(self.depth):
+            if int(response.meta['depth']) < int(self.depth):
                 for url in all_urls:
                     yield Request('%s' % url, callback=self.parse_req, meta={'url': url})
             if len(all_urls) > 0:
@@ -67,9 +84,10 @@ def create_crawler_class():
 
         def get_all_links(self, response):
             links = self.le.extract_links(response)
-            str_links = []
+            str_links = set()
             for link in links:
-                str_links.append(link.url)
+                if(link.url not in self.traversedLinks):
+                    str_links.add(link.url)
             return str_links
 
         def clean_options(self):
